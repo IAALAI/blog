43fa5e66ba62286ba97288a622e226172dbcc767
iaalai <iaalai@iaalai.cn>
2025-01-24 05:41:53 +0800

    add tags base

--- eleventy.config.js
+++ eleventy.config.js
@@ -9,8 +9,6 @@ export default function (eleventyConfig) {
         "public" : "/",
         "src/post": "/post"
     });
-    const _posts_ = [],_tags_ = []
-
     eleventyConfig.addPreprocessor("drafts", "md", (data, content) => {
 		if(data.draft || data.hidden) {
 			return false;
@@ -18,14 +16,10 @@ export default function (eleventyConfig) {
         data.last = fs.statSync(data.page.inputPath).mtime; 
 	});
   
-    eleventyConfig.addCollection("posts", async (collectionsApi) => {
-        const posts = collectionsApi.getAll().filter(value => value.filePathStem.indexOf("/post/") == 0);
-        _posts_.length = 0
-        const tagSet = new Set();
+    eleventyConfig.addCollection("posts",(collectionsApi) => {
+        const posts = collectionsApi.getAll().filter(value => value.filePathStem.indexOf("/post/") == 0),
+            _posts_ = []
         for(let i = 0; i < posts.length; i++) {
-            if (posts[i].data.tags) {
-                tagSet.add(...posts[i].data.tags)
-            }
             _posts_.push({
                 title: posts[i].data.title,
                 tags: posts[i].data.tags,
@@ -34,14 +28,48 @@ export default function (eleventyConfig) {
                 raw: posts[i].rawInput
             })
         }
-        _tags_.length = 0
-        _tags_.push(...tagSet);
         fs.writeFileSync("./dist/assets/search.json", JSON.stringify(_posts_, null, 0), 'utf-8');
         return posts;
     });
-    eleventyConfig.addGlobalData("tags",async () => _tags_);
+    eleventyConfig.addCollection("archive",(collectionsApi) => {
+        const posts = collectionsApi.getAll().filter(value => value.filePathStem.indexOf("/post/") == 0),_archives_ = {};
+        for (let i = 0; i < posts.length; i++) {
+            let a = posts[i].data.categories
+
+            if (!_archives_[posts[i].date.getFullYear()]) _archives_[posts[i].date.getFullYear()] = [];
+            if (!_archives_[posts[i].date.getFullYear()][posts[i].date.getMonth()]) 
+                _archives_[posts[i].date.getFullYear()][posts[i].date.getMonth()] = [];
+            _archives_[posts[i].date.getFullYear()][posts[i].date.getMonth()].push(posts[i])
+        }
+        const archives = [];
+        for (const key in _archives_) {
+            archives.push({
+                key,
+                value: _archives_[key]
+            })
+        }
+        return archives
+    });
+    eleventyConfig.addCollection("tags",(collectionsApi) => {
+        const posts = collectionsApi.getAll().filter(value => value.filePathStem.indexOf("/post/") == 0),_tags_ = {};
+        for (let i = 0; i < posts.length; i++) {
+            if (posts[i].data.tags?.length) {
+                for (const value of posts[i].data.tags) {
+                    if (!_tags_[value]) _tags_[value] = 0;
+                    _tags_[value]++;
+                }
+            }
+        }
+        const tags = [];
+        for (const key in _tags_) {
+            tags.push({
+                key,
+                value: _tags_[key]
+            })
+        }
+        return tags
+    });
     eleventyConfig.addNunjucksFilter("json", (v) => JSON.stringify(v));
-    eleventyConfig.addNunjucksFilter("datep", (v) => `${v.getFullYear()}-${v.getMonth() + 1}-${v.getDate()} ${v.getHours()}:${v.getMinutes()} ${days_p[v.getDay()]} ${month_p[v.getMonth()]}`);
     eleventyConfig.addNunjucksFilter("text", (v) => v.replace(/\<.+?\>/g, ""));
     eleventyConfig.addNunjucksFilter("size", (v) => v.length);
     eleventyConfig.addNunjucksFilter("math", function(source, mode, ...args) {
@@ -53,7 +81,17 @@ export default function (eleventyConfig) {
             return Math[mode](source, ...args);
         }
     });
-    
+    eleventyConfig.addNunjucksFilter("datep",function (v) {
+        if (!v instanceof Date) return v;
+        const Year = v.getFullYear(),
+            Month = v.getMonth(),
+            Day = v.getDate(),
+            Hours = v.getHours(),
+            Minutes = v.getMinutes(),
+            weak_it = v.getDay();
+        return `${Year}-${Month + 1}-${Day} ${Hours}:${Minutes} ${days_p[weak_it]} ${month_p[Month]}`;
+    });
+
     return {
         dir: {
             input: "src",
--- public/script/update.js
+++ public/script/update.js
@@ -1,14 +1,11 @@
 const up_time = document.querySelector(".up_time");
 function update_up_time() {
     const current_time = new Date();
-    const diff = current_time - create_date;
-    up_time.innerHTML = `Up time: 
-        ${Math.floor(diff / (1000 * 60 * 60 * 24))}days,
-        ${Math.floor((diff / (1000 * 60 * 60)) % 24)} hours,
-        ${Math.floor((diff / (1000 * 60)) % 60)} minutes,
-        ${Math.floor((diff / 1000) % 60)} seconds`;
+    if (create_date == null) return;
+    const diff = (current_time - create_date) / 1000;
+    up_time.innerText = `Up time: ${Math.floor(diff / (60 * 60 * 24))}days, ${Math.floor(diff % 86400)} seconds`;
+    up_time.title = `${Math.floor((diff / (60 * 60)) % 24)} hours, ${Math.floor((diff / 60) % 60)} minutes`
 }
-update_up_time();
 let update_Interval = null;
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
